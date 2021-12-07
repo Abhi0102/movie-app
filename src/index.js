ca69d6063f12106390778f9638b3014c9cce9f13
Abhi0102 <abhishek1sharma14.1998@gmail.com>
2021-12-07 12:00:07 +0530

    Middleware logger added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 import './index.css';
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
-import combineReducers from './reducers';
+import rootReducer from './reducers';
 
+//Currying logger
+const logger = function({dispatch, getState}){
+  return function(next){
+    return function(action){
+      // Middleware Operations
+      console.log("Action_Type = ", action.type);
+      next(action);
+    }
+  }
+}
 
-const store = createStore(combineReducers);
+const store = createStore(rootReducer, applyMiddleware(logger));
 ReactDOM.render(
   <React.StrictMode>
     <App store ={store}/>
